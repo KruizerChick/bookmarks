7ebda2ea2924d27988bf9f31730e779a5ec1629c
KruizerChick <KruizerChick@gmail.com>
2018-03-30 10:13:19 -0500

    Add activity stream for tracking user actions.

---
+++ README.md
@@ -0,0 +1,26 @@
+# Bookmarks Web App
+
+Created by following the Udemy tutorial by Packt called [Modern Web Development with Django](https://www.udemy.com/learning-path-django-modern-web-development-with-django/learn/v4/overview).
+
+## Features
+
+This app includes the following features:
+
+- Registration and User Profiles
+- Social Authentication using social-auth-app-django
+- Ability to bookmark and save images from other websites
+- Use of thumbnails to display images
+- Ability to "like" and "dislike" items
+- Pagination via delayed loading using AJAX
+- Ability to "follow" other users
+- An Activity Stream to track what other users are doing.
+- Improved query performance by de-normalizing data (making it redundant) and updating the redundant data using Django signals.
+
+## Requirements
+
+This Bookmarks app is built with:
+
+- Django 2.0.3
+- Pillow
+- social-auth-app-django
+- sorl-thumbnail
--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.models import User
 from django.views.decorators.http import require_POST
 
 from bookmarks.common.decorators import ajax_required
+from bookmarks.actions.utils import create_action
+from bookmarks.actions.models import Action
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile, Contact
 
@@ -44,6 +46,7 @@ def register(request):
             new_user.save()
             # Create the user profile
             profile = Profile.objects.create(user=new_user)
+            create_action(new_user, 'has created an account')
             return render(request,
                           'account/register_done.html',
                           {'new_user': new_user})
@@ -76,8 +79,20 @@ def edit(request):
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', 
-        {'section': 'dashboard'})
+    # Display all actions by default
+    actions = Action.objects.all().exclude(user=request.user)
+    following_ids = request.user.following.values_list(
+        'id', flat=True)
+    if following_ids:
+        # If user is following others, retrieve only their actions
+        actions = actions.filter(
+            user_id__in=following_ids).select_related(
+                'user', 'user__profile').prefetch_related('target')
+    # Only show 10 actions
+    actions = actions[:10]
+
+    return render(request, 'account/dashboard.html', {'section': 'dashboard',
+                                                      'actions': actions})
 
 
 @login_required
@@ -106,7 +121,7 @@ def user_follow(request):
             if action == 'follow':
                 Contact.objects.get_or_create(user_from=request.user,
                                               user_to=user)
-                # create_action(request.user, 'is following', user)
+                create_action(request.user, 'is following', user)
             else:
                 Contact.objects.filter(user_from=request.user,
                                        user_to=user).delete()
